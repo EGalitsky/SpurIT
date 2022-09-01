e468dac7af949a81423d3d6495f962e5e108e335
Evgeny Galitsky <e.galitsky@bk.ru>
2022-09-01 18:23:39 +0300

    add 2 test task

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,45 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const button = document.querySelector("button");
+const text = document.getElementsByClassName("collapsible__content")[0];
+const hideTextButton = document.getElementsByClassName(
+  "collapsible__action--visible"
+)[0];
+const showTextButton = document.getElementsByClassName(
+  "collapsible__action--hidden"
+)[0];
+
+hideTextButton.style.display = "none";
+text.style.display = "none";
+
+const animation = document.querySelector("div").animate(
+  {
+    color: ["rgba(0, 0, 0, 0.000001)", "rgba(0, 0, 0, 1)"],
+  },
+  { duration: 1500, fill: "both", easing: "ease-out" }
+);
+
+const textShowHide = () => {
+  if (showTextButton.style.display == "none") {
+    hideTextButton.style.display = "none";
+    showTextButton.style.display = "";
+  } else {
+    hideTextButton.style.display = "";
+    text.style.display = "";
+    showTextButton.style.display = "none";
+  }
+
+  animation.onfinish = (evt) => {
+    if (evt.currentTime === 0) text.style.display = "none";
+  };
+};
+
+animation.pause();
+
+button.addEventListener("click", () => {
+  console.log(animation.playState);
+  if (animation.playState === "paused") {
+    animation.play();
+  } else {
+    animation.reverse();
+  }
+  textShowHide();
+});
